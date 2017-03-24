ead6242dc6e902972038e959a3bd34300d04e105
Brittany Chiang <brittany.chiang@gmail.com>
2017-03-24 18:53:39 -0400

    login and frame

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,11 @@ const gulp    = require('gulp');
 const sass    = require('./build/sass');
 const scripts = require('./build/scripts');
 const images  = require('./build/images');
+const fonts   = require('./build/fonts');
 const sync    = require('./build/browsersync');
 
-[ sass, scripts, images, sync ].forEach( task => {
+[ sass, scripts, images, fonts, sync ].forEach( task => {
   task( gulp );
 });
 
-gulp.task('build', ['sass', 'scripts', 'images', 'jekyll-build']);
+gulp.task('build', ['sass', 'scripts', 'images', 'fonts', 'jekyll-build']);
